63922e652f04b427c49db475be60ab877039d575
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-06-08 05:23:04 -0400

    Create main.cs

---
+++ miscellaneousAglorithms/binarySearch/main.cs
@@ -0,0 +1,47 @@
+// both approaches to binary search assumes array passed into the function is sorted
+// to increase completeness of algorithm, would need to sort the array passed in before
+// continuing with rest of the algorithm
+
+// iterative T - O(logn)
+public class BinarySearchIterative {
+  public static bool BinarySearch(int[] arr, int targetValue){
+    int low = 0;
+    int high = arr.Length - 1;
+    int guess = 0;
+    int middle = 0;
+    
+    while(low <= high){
+      middle = low + high;
+      guess = arr[middle];
+      
+      if(guess == targetValue){
+        return true;
+      }
+      
+      if(guess > targetValue){
+        high = middle - 1;
+      } else {
+        low = middle + 1;
+      }
+    }
+    
+    return false;
+  }
+}
+
+
+// recursive T - O(logn)
+// returns index of array where targetValue is located if found
+// otherwise returns -1 if not found
+public class BinarySearchRecursive {
+  public static int BinarySearch(int[] arr, int targetValue, int low, int high){
+    if(left > right) return -1;
+    int middle = (left + right) / 2;
+    if(targetValue == arr[middle]) return middle;
+    
+    if(targetValue > arr[middle]){
+      return BinarySearch(arr, targetValue, middle + 1, high);
+    } else {
+      return BinarySearch(arr, targetValue, left, middle - 1):
+  }
+}
