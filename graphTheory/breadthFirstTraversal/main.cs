15552e0ee4c2d86738c872293e8fc1f6df5ebbe6
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-06-12 03:10:08 -0400

    Added DFS implementation with LinkedList adjacency list

--- graphTheory/breadthFirstTraversal/main.cs
+++ graphTheory/breadthFirstTraversal/main.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using System.Collections.Generic;
 
 // T - O(V + E)
@@ -74,4 +75,106 @@ public class BreadFirstTraversalGraph {
             }
         }
     }
+}
+
+// BFS with an adjacency list that is an array of linked lists
+public class BreadthFirstTraversalLinkedListGraph{
+    class LinkedList {
+        class Node {
+            int value;
+            Node next;
+            public Node(int value){
+                this.value = value;
+                this.next = null;
+            }
+        }
+        
+        private Node head;
+        private int size;
+        
+        public LinkedList(){
+            head = null;
+        }
+        
+        public Node GetHead(){
+            Node retrievedNode = head;
+            head = head.next;
+            return retrievedNode;
+        }
+        
+        public void AddAtHead(int value){
+            Node newNode = new Node(value);
+            newNode.next = head;
+            head = newNode;
+            size++;
+        }
+        
+        public int GetSize(){
+            return size;
+        }
+   }
+    
+   class Graph {
+        private int vertices;
+        private LinkedList[] adjacencyList;
+       
+        public Graph(int vertices){
+            this.vertices = vertices;
+            adjacencyList = new LinkedList[vertices];
+            
+            for(int i = 0; i < vertices; i++){
+                adjacencyList[i] = new LinkedList();
+            }
+        }
+       
+        // this is to add edges to a directed graph. Undirected, just need to take out comments for commented out line in method
+        public void AddEdge(int source, int destination){
+            if(source < vertices && destination < vertices){
+                adjacencyList[source].AddAtHead(destination);
+                // adjacencyList[destination].AddAtHead(source);
+            }
+        }
+       
+        public int GetVertices(){
+            return vertices;
+        }
+       
+        public LinkedList[] GetAdjacencyList(){
+            return adjacencyList;
+        }
+   }
+    
+   public static string DFSGeneratedStringFromGraph(Graph adjacencyList, int source){
+        if(source > adjacencyList.GetVertices()){
+            Console.WriteLine("Source entered does not exist in graph of this size. Terminating function call");
+            return;
+        }
+       
+        Queue<int> queue = new Queue<int>();
+        int vertices = adjacencyList.GetVertices();
+        bool[] visited = new bool[vertices];
+        StringBuilder result = new StringBuilder();
+       
+        for(int i = 0; i < vertices; i++){
+            visited[i] = false;
+        }
+       
+        visited[source] = true;
+        queue.Enqueue(source);
+       
+        while(queue.Count != 0){
+            int vertex = queue.Dequeue();
+            result.Append(vertex);
+            LinkedList.Node current = g.GetAdjacencyList()[current].GetHead();
+            while(current != null){
+                if(!visited[current.value]){
+                    visited[current.value] = true;
+                    queue.Enqueue(current.value);
+                }
+                current = current.next;
+            }
+        }
+       
+        return result.ToString();
+   }
 }
