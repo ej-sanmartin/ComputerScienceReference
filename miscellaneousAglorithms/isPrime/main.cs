8c23c39d32697c5fd606768930cf8585a1d81454
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-06-23 05:27:59 -0400

    Update main.cs

--- miscellaneousAglorithms/isPrime/main.cs
+++ miscellaneousAglorithms/isPrime/main.cs
@@ -1,8 +1,8 @@
 class MathHelper {
   public bool isPrime(int n){
     int squaredN = n * n;
-    for(int i = 2; i < n; i++){
-      for(int j = 1; j < squaredN; j++){
+    for(int i = 2; i <= n; i++){
+      for(int j = 1; j <= squaredN; j++){
         if(i * j == n) return false;
       }
     }
