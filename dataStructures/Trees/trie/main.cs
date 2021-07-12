5c493c98abba95110336853a20a83e89e5266910
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-07-12 11:14:43 -0400

    Update time complexity and removed developer helper function

--- dataStructures/Trees/trie/main.cs
+++ dataStructures/Trees/trie/main.cs
@@ -17,10 +17,7 @@ public class Trie {
     root = new Node();
   }
 
-  public void confirm(){
-    Console.WriteLine("Trie Initialize");
-  }
-
+  // O(l), where L is the length of the word you are inserting
   public void Insert(string word){
     InsertHelper(root, word, 0);
   }
@@ -40,6 +37,7 @@ public class Trie {
     InsertHelper(root.children[c], word, index + 1);
   }
 
+  // O(l), where L is the size of the word you are searching for
   public bool Find(string word){
     return FindHelper(root, word, 0);
   }
@@ -55,6 +53,7 @@ public class Trie {
     return FindHelper(root.children[c], word, index + 1);
   }
 
+  // O(l) where L is the length of the word you are trying to delete
   public void Delete(string word){
     DeleteHelper(root, word, 0);
   }
