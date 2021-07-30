d574e553cf9091bc006b7b2b916856ecb84fe4d4
Edgar Jr. San Martin <edgar.sanmartinjr@gmail.com>
2021-07-29 21:57:57 -0400

    Added more functions and fixed minor bugs accessing items in adjacency list

--- graphTheory/graphConversion/main.cs
+++ graphTheory/graphConversion/main.cs
@@ -35,8 +35,8 @@ class GraphConversion {
 
     for(int vertex = 0; vertex < vertices; vertex++){
       for(int edge = 0; edge < graph[vertex].Count; edge++){
-        matrix[vertex, graph[vertex][edge[0]]] = graph[vertex][edge[1]];
-        matrix[graph[vertex][edge[0]], vertex] = graph[vertex][edge[1]];
+        matrix[vertex, graph[vertex][edge][0]] = graph[vertex][edge][1];
+        matrix[graph[vertex][edge][0], vertex] = graph[vertex][edge][1];
       }
     }
 
@@ -49,7 +49,7 @@ class GraphConversion {
 
     for(int vertex = 0; vertex < vertices; vertex++){
       for(int edge = 0; edge < graph[vertex].Count; edge++){
-        matrix[vertex, graph[vertex][edge[0]]] = graph[vertex][edge[1]];
+        matrix[vertex, graph[vertex][edge][0]] = graph[vertex][edge][1];
       }
     }
 
@@ -82,7 +82,7 @@ class GraphConversion {
     int[,] graph = new int[vertices, vertices];
 
     for(int i = 0; i < edges.GetLength(0); i++){
-      graph[edges[i, 0] - 1, edges[i, 1] - 1] = edges[i, 2];
+      graph[edges[i, 0], edges[i, 1]] = edges[i, 2];
     }
 
     return graph;
@@ -117,7 +117,7 @@ class GraphConversion {
     Dictionary<int, List<int[]>> graph = new Dictionary<int, List<int[]>>();
 
     for(int vertex = 0; vertex < vertices; vertex++){
-      graph.Add(vertex, new List<int>());
+      graph.Add(vertex, new List<int[]>());
     }
 
     for(int vertex = 0; vertex < edges.GetLength(0); vertex++){
@@ -129,7 +129,7 @@ class GraphConversion {
   }
 
   public static Dictionary<int, List<int>> EdgeListToUnweightedDirectedAdjacencyList(int[,] edges, int vertices){
-    Dictionary<int, List<int>> graph = new Dictonary<int, List<int>>();
+    Dictionary<int, List<int>> graph = new Dictionary<int, List<int>>();
 
     for(int vertex = 0; vertex < vertices; vertex++){
       graph.Add(vertex, new List<int>());
@@ -154,5 +154,113 @@ class GraphConversion {
     }
 
     return graph;
+  }
+
+  public static List<int[]> WeightedDirectedAdjacencyListToEdgeList(Dictionary<int, List<int[]>> graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      foreach(int[] edge in graph[vertex]){
+        edgeList.Add(new int[]{ vertex, edge[0], edge[1] });
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> WeightedUndirectedAdjacencyListToEdgeList(Dictionary<int, List<int[]>> graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      foreach(int[] edge in graph[vertex]){
+        edgeList.Add(new int[]{ vertex, edge[0], edge[1] });
+        edgeList.Add(new int[]{ edge[0], vertex, edge[1] });
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> UnweightedDirectedAdjacencyListToEdgeList(Dictionary<int, List<int>> graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      foreach(int edge in graph[vertex]){
+        edgeList.Add(new int[]{ vertex, edge});
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> UnweightedUndirectedAdjacencyListToEdgeList(Dictionary<int, List<int>> graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      foreach(int edge in graph[vertex]){
+        edgeList.Add(new int[]{ vertex, edge});
+        edgeList.Add(new int[]{ edge, vertex});
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> WeightedDirectedAdjacencyMatrixToEdgeList(int[,] graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      for(int edge = 0; edge < graph.GetLength(1); edge++){
+        if(graph[vertex, edge] != 0){
+          edgeList.Add(new int[]{ vertex, edge, graph[vertex, edge] });
+        }
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> WeightedUndirectedAdjacencyMatrixToEdgeList(int[,] graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      for(int edge = 0; edge < graph.GetLength(1); edge++){
+        if(graph[vertex, edge] != 0){
+          edgeList.Add(new int[]{ vertex, edge, graph[vertex, edge] });
+          edgeList.Add(new int[]{ edge, vertex, graph[vertex, edge] });
+        }
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> UnweightedDirectedAdjacencyMatrixToEdgeList(int[,] graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      for(int edge = 0; edge < graph.GetLength(1); edge++){
+        if(graph[vertex, edge] != 0){
+          edgeList.Add(new int[]{ vertex, edge });
+        }
+      }
+    }
+
+    return edgeList;
+  }
+
+  public static List<int[]> UnweightedDirectedAdjacencyMatrixToEdgeList(int[,] graph){
+    List<int[]> edgeList = new List<int[]>();
+
+    for(int vertex = 0; vertex < graph.Count; vertex++){
+      for(int edge = 0; edge < graph.GetLength(1); edge++){
+        if(graph[vertex, edge] != 0){
+          edgeList.Add(new int[]{ vertex, edge });
+          edgeList.Add(new int[]{ edge, vertex });
+        }
+      }
+    }
+
+    return edgeList;
   }
 }
